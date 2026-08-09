7d42a17c33af6d7bfb8bc6c00e767886c5d67823
Paulo Castellano <paulo@castellanos.llc>
2026-08-08 22:26:18 -0300

    Add social icons and copyright to email footers
    Icons match the trypost-site footer (outline @tabler/icons style,
    converted to PNG since email clients — notably Outlook desktop — don't
    render inline SVG). Reordered footer content: tagline, manage-notifications
    link, icons as the closing element, copyright line last.

--- resources/views/mail/workspace-invite.blade.php
+++ resources/views/mail/workspace-invite.blade.php
@@ -98,6 +98,38 @@
             <p style="margin: 0 0 8px">
               Open-source social media scheduling tool
             </p>
+            <table role="presentation" cellpadding="0" cellspacing="0" border="0" style="margin-left: auto; margin-right: auto; margin-top: 12px">
+              <tr>
+                <td style="padding-left: 4px; padding-right: 4px">
+                  <a href="https://github.com/trypostit/trypost" target="_blank">
+                    <img src="{{ asset('/images/emails/social/github.png') }}" width="20" height="20" alt="GitHub" style="max-width: 100%; vertical-align: middle">
+                  </a>
+                </td>
+                <td style="padding-left: 4px; padding-right: 4px">
+                  <a href="https://x.com/trypostit" target="_blank">
+                    <img src="{{ asset('/images/emails/social/x.png') }}" width="20" height="20" alt="X" style="max-width: 100%; vertical-align: middle">
+                  </a>
+                </td>
+                <td style="padding-left: 4px; padding-right: 4px">
+                  <a href="https://www.youtube.com/@trypostit" target="_blank">
+                    <img src="{{ asset('/images/emails/social/youtube.png') }}" width="20" height="20" alt="YouTube" style="max-width: 100%; vertical-align: middle">
+                  </a>
+                </td>
+                <td style="padding-left: 4px; padding-right: 4px">
+                  <a href="https://trypost.it/discord" target="_blank">
+                    <img src="{{ asset('/images/emails/social/discord.png') }}" width="20" height="20" alt="Discord" style="max-width: 100%; vertical-align: middle">
+                  </a>
+                </td>
+                <td style="padding-left: 4px; padding-right: 4px">
+                  <a href="https://www.instagram.com/trypost.it" target="_blank">
+                    <img src="{{ asset('/images/emails/social/instagram.png') }}" width="20" height="20" alt="Instagram" style="max-width: 100%; vertical-align: middle">
+                  </a>
+                </td>
+              </tr>
+            </table>
+            <p style="margin: 12px 0 0">
+              &copy; {{ date('Y') }} TryPost.it
+            </p>
           </td>
         </tr>
       </table>
